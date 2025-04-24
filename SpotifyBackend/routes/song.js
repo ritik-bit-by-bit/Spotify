aefcbe678bb1d45ff12b87c2247a4b3ae345a10d
ritik-bit-by-bit <ritikroshanyadav9696@gmail.com>
2025-04-24 23:02:15 +0530

    added song routes for fetching song by name and artist

--- SpotifyBackend/routes/song.js
+++ SpotifyBackend/routes/song.js
@@ -29,4 +29,27 @@ router.get('/get/mysong',passport.authenticate("jwt",{session:false}),async(req,
       console.log(songs);
       return res.status(200).json(songs);
 })
+
+router.get('get/artist',passport.authenticate("jwt",{session:false}),async(req,res)=>{
+  console.log(req.user);
+  const artistId = req.body;
+  const artist= await UserModel.find({_id:artistId});
+  console.log(artist)
+  if(!artist){
+    return res.status(404).json({msg:"Artist not found"})
+  }
+  const songs = await SongModel.find({Artist:artistId});
+  console.log(songs);
+  return res.status(200).json(songs);
+})
+
+router.get("get/name",passport.authenticate("jwt",{session:false}),async(req,res)=>{
+   const songName = req.body;
+   const song = await SongModel.find({SongName:songName});
+   if(!song){
+    return res.status(404).json({msg:"Song not found"})
+   }
+   return res.status(200).json(song);
+})
+
 module.exports=router; 
