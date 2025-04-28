32cba60002f0da33a8178785abc183b317e39c6c
ritik-bit-by-bit <ritikroshanyadav9696@gmail.com>
2025-04-28 21:06:09 +0530

    improved the login page ui

--- SpotifyFrontend/src/Components/Label.jsx
+++ SpotifyFrontend/src/Components/Label.jsx
@@ -1,10 +1,10 @@
 const Label = ({type,title, inputmessage})=>{
     return (
-       <div className="w-full className='pt-5'">
-          <div className="text-md font-bold pl-2 ">
+       <div className="w-full pt-5 ">
+          <div className="text-md font-bold pl-3 ">
             {title}
           </div>
-          <div className="pt-0.5 pb-2 pl-2 pr-16 ">
+          <div className="pt-0.5 pb-2 pl-3 pr-6 ">
             <input type={type} required={true} placeholder={inputmessage} className="border border-solid border-gray-300 w-full pt-1 pb-1 rounded-sm"></input>
           </div>
        </div>
